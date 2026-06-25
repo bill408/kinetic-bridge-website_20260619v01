cd09af3fc3e522f952732be9802a548472cb78c4
Bill Bevco <bill@bevco-tech.com>
2026-06-25 08:48:00 -0700

    Update vite.config.js
    update for v0 app to work

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['sb-7fsrqk6i0g7z.vercel.run'],
+  },
 })
